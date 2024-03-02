6a28cb965a356c0180db105a026d1e1b9b0f2763
parteeksingh24 <parteek.singh24@gmail.com>
2024-03-01 16:17:59 -0800

    Updated classification script
    Incorporated LSTM, GRU models

--- script/stage_4_script/classification.py
+++ script/stage_4_script/classification.py
@@ -2,6 +2,8 @@
 from code.stage_4_code.Classification_Dataset_Loader import Classification_Dataset_Loader
 from code.stage_4_code.Result_Saver import Result_Saver
 from code.stage_4_code.Method_Classification_Rnn import Method_Classification_RNN
+#from code.stage_4_code.Method_Classification_LSTM import Method_Classification_LSTM
+#from code.stage_4_code.Method_Classification_GRU import Method_Classification_GRU
 from code.stage_4_code.Setting_Movie_Classification import Setting_Movie_Classification
 from code.stage_4_code.Evaulate_Classification_Metrics import Evaluate_Classification_Metrics
 import os
@@ -59,6 +61,8 @@ if not os.path.exists(result_obj.result_destination_folder_path):
 result_obj.result_destination_file_name = 'RNN_classification_result'
 
 method_obj = Method_Classification_RNN("Classification RNN", '', result_obj.result_destination_folder_path, vocab_size, embedding_size, hidden_size, num_rnn_layers, output_size, learning_rate, batch_size, loss_function, optimizer, max_epochs, text_length=text_length)
+# method_lstm_obj = Method_Classification_LSTM("Classification LSTM", '', result_obj.result_destination_folder_path, vocab_size, embedding_size, hidden_size, num_rnn_layers, output_size, learning_rate, batch_size, loss_function, optimizer, max_epochs, text_length=text_length)
+# method_gru_obj = Method_Classification_GRU("Classification GRU", '', result_obj.result_destination_folder_path, vocab_size, embedding_size, hidden_size, num_rnn_layers, output_size, learning_rate, batch_size, loss_function, optimizer, max_epochs, text_length=text_length)
 
 setting_obj = Setting_Movie_Classification('classification', '')
 
@@ -68,6 +72,9 @@ evaluate_obj = Evaluate_Classification_Metrics('metrics', '')
 # # ---- running section ---------------------------------
 print('************ Start ************')
 setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+# setting_obj.prepare(data_obj, method_lstm_obj, result_obj, evaluate_obj)
+# setting_obj.prepare(data_obj, method_gru_obj, result_obj, evaluate_obj)
+
 setting_obj.print_setup_summary()
 result = setting_obj.load_run_save_evaluate()
 print('************ Overall Performance ************')
